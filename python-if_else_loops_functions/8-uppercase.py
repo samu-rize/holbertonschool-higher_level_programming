5333756604ae0343738edc5d9ca614514f2b250e
samu-rize <6710@holbertonstudents.com>
2023-10-07 16:53:56 +0100

    To uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strout = ''
+    for ch in str:
+        if ord(ch) in range (97, 123):
+            strout += chr(ord(ch) - 32)
+        else:
+            strout += ch
+    print(strout)
