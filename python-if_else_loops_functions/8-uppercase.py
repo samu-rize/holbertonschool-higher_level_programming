26fca4d48ccab84de69c30a3016e05638ed0c877
samu-rize <6710@holbertonstudents.com>
2023-10-07 17:00:30 +0100

    fix pycodestyle error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     strout = ''
     for ch in str:
-        if ord(ch) in range (97, 123):
+        if ord(ch) in range(97, 123):
             strout += chr(ord(ch) - 32)
         else:
             strout += ch
