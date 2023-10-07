8bca25f658ce8de466ec3b60dcb0f301c04d123c
samu-rize <6710@holbertonstudents.com>
2023-10-07 18:50:42 +0100

    add prototype

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if (i % 3) and (i % 5):
-        print('{}'.format(i), end=' ')
-    elif not (i % 3) and (i % 5):
-        print('Fizz', end=' ')
-    elif (i % 3) and not (i % 5):
-        print('Buzz', end=' ')
-    else:
-        print('FizzBuzz', end=' ')
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 3) and (i % 5):
+            print('{}'.format(i), end=' ')
+        elif not (i % 3) and (i % 5):
+            print('Fizz', end=' ')
+        elif (i % 3) and not (i % 5):
+            print('Buzz', end=' ')
+        else:
+            print('FizzBuzz', end=' ')
