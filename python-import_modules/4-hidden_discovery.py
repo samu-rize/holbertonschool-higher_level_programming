d6048e3b0868f8396ebb8c3e0461151a1e7468e4
samu-rize <6710@holbertonstudents.com>
2023-10-20 14:17:13 +0100

    Infinite addition

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def main():
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != '__':
+            print(name)
+
+
+if __name__ == "__main__":
+    main()
