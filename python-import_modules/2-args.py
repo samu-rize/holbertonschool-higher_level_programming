4d3f86a511fa25e2000edfd995a7ecdc624f8774
samu-rize <6710@holbertonstudents.com>
2023-10-20 11:37:31 +0100

    correct the output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
     if argc == 0:
-        print('0 argements.')
+        print('0 arguments.')
     elif argc == 1:
-        print('1 argement:')
+        print('1 argument:')
     else:
-        print('{} argemenets:'.format(argc))
+        print('{} argumenets:'.format(argc))
     for i in range(argc):
         print('{}: {}'.format(i+1, sys.argv[i+1]))
