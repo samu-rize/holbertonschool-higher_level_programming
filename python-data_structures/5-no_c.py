502b4508f29c17057034974412957b523a44b019
samu-rize <6710@holbertonstudents.com>
2023-10-22 21:37:53 +0100

    Can you C me now?

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for i in range(len(my_string)):
+        if my_string[i] not in 'Cc':
+            new_string += my_string[i]
+            i -= 1
+    return new_string
