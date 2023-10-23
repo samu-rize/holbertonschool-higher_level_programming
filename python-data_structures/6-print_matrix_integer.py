c9f3afe65f7e8f54426a82d40cc5eb1b59a591e2
samu-rize <6710@holbertonstudents.com>
2023-10-23 11:43:22 +0100

    Can you C me now?

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    lenx = len(matrix)
+    for x in range(lenx):
+        leny = len(matrix[x])
+        for y in range(leny):
+            if y == leny - 1:
+                print('{:d}'.format(matrix[x][y]))
+            else:
+                print('{:d}'.format(matrix[x][y]), end=' ')
