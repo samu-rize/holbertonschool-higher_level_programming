ae578a325eff8f77bcef20d4e51f7e8342136a20
samu-rize <6710@holbertonstudents.com>
2023-10-23 21:02:01 +0100

    put the output in a list

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new = []
     for i in range(len(my_list)):
         if (my_list[i] % 2):
-            return False
+            new.append(False)
         else:
-            return True
+            new.append(True)
+    return new
