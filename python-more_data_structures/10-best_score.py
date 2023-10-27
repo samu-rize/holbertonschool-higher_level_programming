93eb0e3f77c2262d4e20136dfc6f047de5cfb80e
samu-rize <6710@holbertonstudents.com>
2023-10-27 22:10:11 +0100

    initialize best_val as "0" and remove the second none condition

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,11 +4,11 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    best_val = None
+    best_val = 0
     best_key = ''
 
     for key, val in a_dictionary.items():
-        if best_val is None or val > best_val:
+        if val > best_val:
             best_val = val
             best_key = key
     return best_key
