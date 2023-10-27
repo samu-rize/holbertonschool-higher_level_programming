c8960d8cf9c957b6c5dc0666b360a01e94b40c74
samu-rize <6710@holbertonstudents.com>
2023-10-27 21:51:29 +0100

    10. Best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+
+    if a_dictionary is None:
+        return None
+
+
+    best_val = None
+
+
+    for key, val in a_dictionary.items():
+        if best_val == None or val > best_val:
+            best_val = val
+            best_key = key
+    return best_key
