d1f2bf590dd408222146514ccd7a904e8604ef98
samu-rize <6710@holbertonstudents.com>
2023-10-27 21:57:14 +0100

    2 additional blank lines removed and replace "==" by "is"

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,12 +4,10 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-
     best_val = None
 
-
     for key, val in a_dictionary.items():
-        if best_val == None or val > best_val:
+        if best_val is None or val > best_val:
             best_val = val
             best_key = key
     return best_key
