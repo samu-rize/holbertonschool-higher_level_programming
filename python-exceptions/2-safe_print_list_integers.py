fba88815806b7648f813d39bba2f91811557f119
samu-rize <6710@holbertonstudents.com>
2023-10-28 14:13:41 +0100

    fix 'Expression value is unused' by change none by pass

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print('{:d}'.format(my_list[idx]), end='')
             count += 1
         except (TypeError, ValueError):
-            None
+            pass
     print()
     return count
