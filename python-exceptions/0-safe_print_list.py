9ae1d8da3b214fa6f0dcb737d7d8bc83c01e1c74
samu-rize <6710@holbertonstudents.com>
2023-10-28 11:58:54 +0100

    specify the type of the exeption

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     for idx in range(x):
         try:
             print(my_list[idx], end='')
-        except:
+        except IndexError:
             print('')
             return idx
 
