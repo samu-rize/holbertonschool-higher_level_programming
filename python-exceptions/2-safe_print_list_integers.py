88a9e509ab5073d75fe7e0c94845763307615533
samu-rize <6710@holbertonstudents.com>
2023-10-28 13:40:53 +0100

    specify the types of the exeptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print('{:d}'.format(my_list[idx]), end='')
             count += 1
-        except:
+        except (TypeError, ValueError, IndexError):
             None
     print()
     return count
