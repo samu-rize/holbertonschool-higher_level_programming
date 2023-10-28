0edceecbdee95d1ed8a33c4628272aced4164729
samu-rize <6710@holbertonstudents.com>
2023-10-28 23:14:08 +0100

    0. My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """
+    This is a simple Python class to represent a square.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    """
+    pass
