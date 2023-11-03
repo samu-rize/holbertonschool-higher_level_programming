f4820a8de052cf8fb728159e838c5559e6ca5696
samu-rize <6710@holbertonstudents.com>
2023-11-03 18:17:56 +0100

    add documentations

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,15 +5,18 @@
 class Rectangle:
     """Simple Python class to represent a Rectangle."""
     def __init__(self, width=0, height=0):
+        """Initialize a Rectangle with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the Rectangle."""
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
@@ -22,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height of the Rectangle."""
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
