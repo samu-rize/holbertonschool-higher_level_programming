89be01c03116fe4cc214c260310b4d34886b5f49
samu-rize <6710@holbertonstudents.com>
2023-11-03 16:08:32 +0100

    undo the last modification

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#Rectangle module
+"""Rectangle module"""
 
 
 class Rectangle:
-    #Simple Python class to represent a Rectangle.
+    """Simple Python class to represent a Rectangle."""
     pass
