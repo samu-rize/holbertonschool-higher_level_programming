93c44966e8e08ea7a00d4683b431dd3fae7c31e2
samu-rize <6710@holbertonstudents.com>
2023-11-04 23:15:24 +0100

    fix the pycodestyle error

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -78,7 +78,8 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Static method to compare two rectangles and return the bigger one."""
+        """Static method to compare two rectangles and return
+        the bigger one."""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
