751405a4aadd89344a1c678608554218102b15f4
samu-rize <6710@holbertonstudents.com>
2023-11-12 21:54:26 +0100

    add doccuments

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,9 +5,9 @@
 class BaseGeometry:
     """BaseGeometry class"""
     def __init__(self):
-        """"""
+        """Empty initializer"""
         pass
 
     def area(self):
-        """"""
+        """Placeholder for area method"""
         raise Exception("area() is not implemented")
