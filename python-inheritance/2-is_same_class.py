f8b87b02471bea56eb33ec0bceb5d8ee92f9463f
samu-rize <6710@holbertonstudents.com>
2023-11-12 16:28:27 +0100

    specify the condition to confirm the class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """Check if obj is an instance of a_class"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return a_class == type(obj)
