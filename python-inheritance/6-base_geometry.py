84f463a2c9435ff36f00f799f6d8beaa9438874a
samu-rize <6710@holbertonstudents.com>
2023-11-12 21:48:14 +0100

    6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""BaseGeometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def __init__(self):
+        """"""
+        pass
+
+    def area(self):
+        """"""
+        raise Exception("area() is not implemented")
