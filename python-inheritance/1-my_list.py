b10066509d782b058c22425808fae3e2dacc540e
samu-rize <6710@holbertonstudents.com>
2023-11-12 13:25:32 +0100

    1. My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a simple script with a MyList class."""
+
+
+class MyList:
+    """MyList is a subclass of the built-in list class."""
+    def print_sorted(self):
+        """This method prints the sorted elements of the list."""
+        if issubclass(MyList, list):
+            print(sorted(self))
