2b409ccf959a1d0f61e9202cece87aa9186b48b3
samu-rize <6710@holbertonstudents.com>
2023-11-13 20:20:47 +0100

    Add doctests for

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module contain a functions to add 2 integers"""
+
+
 def add_integer(a, b=98):
     """function add 2 integers"""
     if not (isinstance(a, int) or isinstance(a, float)):
@@ -7,3 +9,7 @@ def add_integer(a, b=98):
     elif not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("{} must be an integer".format(b))
     return int(a) + int(b)
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
