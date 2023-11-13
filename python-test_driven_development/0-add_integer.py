97e1c22bb3c3001e69ff93ab0b3375a5c1e3a199
samu-rize <6710@holbertonstudents.com>
2023-11-13 22:43:49 +0100

    try another format

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@
 
 def add_integer(a, b=98):
     """function add 2 integers"""
-    if not (isinstance(a, int) or isinstance(a, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int) or isinstance(b, float)):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
 
