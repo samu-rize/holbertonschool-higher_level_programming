e5d03ba41b5c1d411feab7f635ab0978a589f937
samu-rize <6710@holbertonstudents.com>
2023-11-13 22:27:20 +0100

    change the writing of the condition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@
 
 def add_integer(a, b=98):
     """function add 2 integers"""
-    if type(a) not in (int, float):
+    if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
-    elif type(b) not in (int, float):
+    elif not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
 
