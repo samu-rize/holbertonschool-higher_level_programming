334daeed44c90b8ef312d1e8ad2b565fecec54a2
samu-rize <6710@holbertonstudents.com>
2023-11-13 20:11:49 +0100

    add doccuments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""module contain a functions to add 2 integers"""
 def add_integer(a, b=98):
+    """function add 2 integers"""
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("{} must be an integer".format(a))
     elif not (isinstance(b, int) or isinstance(b, float)):
