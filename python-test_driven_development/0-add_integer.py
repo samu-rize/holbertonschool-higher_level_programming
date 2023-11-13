68de03fe576bc1249ae6fa736d47e39dda79441e
samu-rize <6710@holbertonstudents.com>
2023-11-13 22:41:31 +0100

    change elif by if

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ def add_integer(a, b=98):
     """function add 2 integers"""
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
-    elif not (isinstance(b, int) or isinstance(b, float)):
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
 
