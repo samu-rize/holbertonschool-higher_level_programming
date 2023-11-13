d6ee7c8c8151e87de3fcec137eae2ccb500cb480
samu-rize <6710@holbertonstudents.com>
2023-11-13 22:16:31 +0100

    remove the extras tabs

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,6 +10,6 @@ def add_integer(a, b=98):
         raise TypeError("{} must be an integer".format(b))
     return int(a) + int(b)
 
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("tests/0-add_integer.txt")
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
