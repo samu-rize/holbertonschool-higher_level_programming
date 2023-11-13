6352caf94ff28c2b8b185b80d0304e4bf04549d2
samu-rize <6710@holbertonstudents.com>
2023-11-13 23:22:01 +0100

    change the doccument

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""module contain a functions to add 2 integers"""
+"""
+            module have a function
+    that returt the sum of 2 integers a and b
+"""
 
 
 def add_integer(a, b=98):
-    """function add 2 integers"""
+    """
+    function that adds 2 integers
+    a and b must be integers or floats if not
+    aise a TypeError exception with the message
+    a must be an integer or b must be an integer
+    """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
