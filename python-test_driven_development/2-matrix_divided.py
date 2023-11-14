1261b42ab84dcab6585721714e2499dab5e24675
samu-rize <6710@holbertonstudents.com>
2023-11-14 22:54:27 +0100

    check if the matrix is empty

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,18 +5,23 @@
 def matrix_divided(matrix, div):
     """functions divides all elements of a matrix"""
     new_matrix = []
+
+    if not matrix:
+        return new_matrix
+
     row_size = len(matrix[0])
+
     if not (isinstance(div, int) or isinstance(div, float)):
         raise TypeError ("div must be a number")
     if div == 0:
         raise ZeroDivisionError ("division by zero")
 
     for i in range(len(matrix)):
-        new_row = []
 
         if row_size != len(matrix[i]):
             raise TypeError ("Each row of the matrix must have the same size")
 
+        new_row = []
         for j in range(len(matrix[i])):
 
             if not (isinstance(matrix[i][j], int) or isinstance(matrix[i][j], float)):
