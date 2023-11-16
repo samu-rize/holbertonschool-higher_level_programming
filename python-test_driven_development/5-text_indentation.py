acd2d32bd38ff3f6086d76d10b4991eabf5da5b1
samu-rize <6710@holbertonstudents.com>
2023-11-16 17:42:04 +0100

    add soctest

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -24,3 +24,7 @@ def text_indentation(text):
                 idx += 1
             continue
         idx += 1
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
