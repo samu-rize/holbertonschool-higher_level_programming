289c723b0afcdd0ae21a0da9bea780934b446251
samu-rize <6710@holbertonstudents.com>
2023-11-16 11:50:07 +0100

    add doctest

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""""""
+"""say_my_name fuction"""
 
 
 def say_my_name(first_name, last_name=""):
+    """print first and last name in a specific format"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
