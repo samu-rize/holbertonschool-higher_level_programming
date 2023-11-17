1d5b109e32eba576057fdbe2e8f8bb41e8743759
samu-rize <6710@holbertonstudents.com>
2023-11-17 12:33:47 +0100

    pycodestyle fixed

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,8 @@
 
 
 def text_indentation(text):
-    """a function that prints a text with 2 new lines after each of these characters: ".", "?", ":" """
+    """a function that prints a text with 2 new lines after each
+    of these characters: ".", "?", ":" """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
