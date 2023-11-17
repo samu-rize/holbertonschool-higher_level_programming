f28454acd0dd564bf97a8500a71122c6f6e3ce4a
samu-rize <6710@holbertonstudents.com>
2023-11-17 14:24:34 +0100

    fix  bugs

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,24 +7,24 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """unitest for max_integer([..])"""
 
-    def Test_no_arg(self):
+    def test_no_arg(self):
         """test max integer with test_no_arg"""
         self.assertEqual(max_integer([]), None)
 
-    def Test_Ascending_ordered_list(self):
-        """test max integer with Test_Ascending_ordered_list"""
+    def test_Ascending_ordered_list(self):
+        """test max integer with test_Ascending_ordered_list"""
         self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
-    def Test_unordered_list(self):
+    def test_unordered_list(self):
         """test max integer with test_unordered_list"""
         self.assertEqual(max_integer([1, 4, 5, 2, 3]), 5)
 
-    def Test_descending_ordered_list(self):
-        """test max integer with Test_descending_ordered_list"""
+    def test_descending_ordered_list(self):
+        """test max integer with test_descending_ordered_list"""
         self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
 
-    def Test_one_element_list(self):
-        """test max integer with Test_one_element_list"""
+    def test_one_element_list(self):
+        """test max integer with test_one_element_list"""
         self.assertEqual(max_integer([5]), 5)
 
     def test_list_of_float(self):
@@ -37,7 +37,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_string(self):
         """test max integer with test_string"""
-        self.assertEqual(max_integer("string"), "r")
+        self.assertEqual(max_integer("string"), "t")
 
     def test_list_of_string(self):
         """test max integer with test_list_of_string"""
@@ -45,7 +45,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_empty_string(self):
         """test max integer with test_empty_string"""
-        self.assertEqual(max_integer(""), "strings")
+        self.assertEqual(max_integer(""), None)
 
 
 if __name__ == "__main__":
