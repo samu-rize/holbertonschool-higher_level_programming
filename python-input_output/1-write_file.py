afbd776dbf55da14ba7a257aef5b714ed5aa997e
samu-rize <6710@holbertonstudents.com>
2023-11-17 21:59:14 +0100

    add doccuments

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""Simple script to write text to a file."""
 
 
 def write_file(filename="", text=""):
-    """"""
+    """Writes the specified text to the given file."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
