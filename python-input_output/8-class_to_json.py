fb068c8a3fd4caf174387ef5c2c52b95386cbf67
samu-rize <6710@holbertonstudents.com>
2023-11-18 18:08:34 +0100

    8. Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Converts a class instance to a JSON-serializable dictionary."""
+
+
+def class_to_json(obj):
+    """Converts the specified class instance to a JSON-serializable dictionary."""
+    return obj.__dict__
