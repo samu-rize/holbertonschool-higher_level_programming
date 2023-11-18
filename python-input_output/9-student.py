796b94caa92cd44cbfb6e9939d6a17e68676a27c
samu-rize <6710@holbertonstudents.com>
2023-11-18 18:35:26 +0100

    9. Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a simple Student class and its to_json method."""
+
+import json
+
+
+class Student:
+    """Represents a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Converts the Student instance to a JSON-serializable dictionary."""
+        return self.__dict__
