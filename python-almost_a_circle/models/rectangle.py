2e1e0bc6e5c3782be6c9a5e9e3fc225ea4fc85a9
samu-rize <6710@holbertonstudents.com>
2023-11-19 14:42:48 +0100

    pycodestyle fixed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,21 +4,23 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor"""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
-        self.y = y 
+        self.y = y
 
     @property
     def width(self):
         """width"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         self.__width = value
@@ -27,26 +29,25 @@ class Rectangle(Base):
     def height(self):
         """height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         self.__height = value
-    
+
     @property
     def x(self):
         """x"""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         self.__x = value
-    
+
     @property
     def y(self):
         """y"""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         self.__y = value
-    
