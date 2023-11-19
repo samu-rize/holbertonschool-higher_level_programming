4a1c03eaf9125b34d093c2ace8f19cad36b74f77
samu-rize <6710@holbertonstudents.com>
2023-11-19 20:33:43 +0100

    update(7. Display #1)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,8 +72,9 @@ class Rectangle(Base):
 
     def display(self):
         """Prints in stdout the Rectangle instance with the character "#" """
+        print("\n" * self.y)
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__,
