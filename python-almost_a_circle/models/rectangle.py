621545567e730c029c853b07082a3cc3d775617a
samu-rize <6710@holbertonstudents.com>
2023-11-19 14:42:03 +0100

    2. First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module for Rectangle class"""
+
+
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y 
+
+    @property
+    def width(self):
+        """width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """height"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+    
+    @property
+    def x(self):
+        """x"""
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        self.__x = value
+    
+    @property
+    def y(self):
+        """y"""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
+    
