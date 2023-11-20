dfe20fe858a6a148225642448ed17381d4515d5d
samu-rize <6710@holbertonstudents.com>
2023-11-20 04:39:47 +0100

    Update (15. Dictionary to JSON string)

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a simple Base class for managing unique IDs."""
 
-
+from json import dumps
 class Base:
     """Represents a base class with a unique ID generator."""
     __nb_objects = 0
@@ -13,3 +13,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Dictionary to JSON string"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
