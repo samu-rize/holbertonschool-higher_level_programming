96af0bf7bbbfaaf630726adcaf69d7a2b34993b6
samu-rize <6710@holbertonstudents.com>
2023-11-20 06:34:15 +0100

    Update (17. JSON string to dictionary)

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a simple Base class for managing unique IDs."""
 
-from json import dumps
+from json import dumps, loads
 
 
 class Base:
@@ -31,3 +31,11 @@ class Base:
             list_objs = [obj.to_dictionary() for obj in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """JSON string to dictionary"""
+        if json_string is None or not json_string:
+            return "[]"
+        else:
+            return loads(json_string)
