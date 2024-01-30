83ce249040e2840e175bc15fd154999149c7606e
samu-rize <6710@holbertonstudents.com>
2024-01-30 13:16:01 +0100

    add ',' in the end of the line

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -8,7 +8,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
